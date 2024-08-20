46c96300b63a12629fbf75e23a0c2dc6cfea0953
sannishamusideen <samsonanu@yahoo.com>
2024-08-20 12:07:06 +0100

    subnav

--- assets/js/app5.js
+++ assets/js/app5.js
@@ -3,6 +3,20 @@ const navbar = document.querySelector('.navbar');
 const navItems = document.querySelectorAll('.navItem');
 console.log(navItems);
 
+
+navItems.forEach((ele) =>{
+    console.log(ele);
+    if(ele.querySelector('.subNav')){
+        ele.addEventListener('click', (e)=>{
+            if(ele.classList.contains('sactive')){ 
+                ele.classList.remove('sactive');
+            }else {
+                ele.classList.add('sactive');
+            }
+        })
+    }
+})
+
 toggler.addEventListener('click',(e)=>{
     if(navbar.classList.contains('active')){
         navbar.classList.remove('active');
